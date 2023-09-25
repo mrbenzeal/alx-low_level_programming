cb9bba0559bce1c45ef8cc3dc61c7b5d4577c197
mrbenzeal <anibenzealobinna@gmail.com>
2023-09-24 22:55:48 -0700

    'I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game'

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function to print alphabets in lower case
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
