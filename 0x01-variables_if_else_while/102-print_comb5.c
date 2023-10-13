67d7e1cd133fe258943b438175e98eca2956abac
mrbenzeal <anibenzealobinna@gmail.com>
2023-10-12 20:16:02 -0700

    "Software is eating the World"

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -7,33 +9,26 @@
  */
 
 int main(void)
+
 {
-	int a1, b1, a2, b2;
+	int a, b;
 
-	for (a1 = 0; a1 < 10; a1++)
+	for (a = 0; a <= 98; a++)
 	{
-		for (b1 = 0; b1 < 10; b1++)
-		{
-			for (a2 = a1; a2 < 10; a2++)
-			{
-				for (b2 = (a1 == a2) ? b1 + 1 : 0; b2 < 10; b2++)
-				{
-					putchar(a1 + '0');
-					putchar(b1 + '0');
-					putchar(' ');
-					putchar(a2 + '0');
-					putchar(b2 + '0');
-					if (!(a1 == 9 && b1 == 9 && a2 == 9 && b2 == 9))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			}
-		}
-	}
 
+	for (b = a + 1; b <= 99; b++)
+	{
+	putchar((a / 10) + '0');
+	putchar((a % 10) + '0');
+	putchar(' ');
+	putchar((b / 10) + '0');
+	putchar((b % 10) + '0');
+	if (a == 98 && b == 99)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
 	putchar('\n');
 	return (0);
 }
-
